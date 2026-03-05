acc7a38f1d621cd12452466aa2c1851c3ffb9ace
Giorgian Robescu <robescu.george96@gmail.com>
2026-03-05 13:33:14 +0200

    Difficulties no longer hard coded

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,17 @@
+// Variables & DOM selections
+const DIFFICULTIES = {
+    easy: { name: 'Easy', size: { width: 9, height: 9 }, numberOfMines: 10 },
+    medium: { name: 'Medium', size: { width: 16, height: 16 }, numberOfMines: 40 },
+    hard: { name: 'Hard', size: { width: 30, height: 16 }, numberOfMines: 99 },
+};
+const difficultySelect = document.getElementById('difficulty');
+// Game logic, functions, event listeners
+Object.entries(DIFFICULTIES).forEach(function ([key, setting]) {
+    const option = document.createElement('option');
+    option.value = key;
+    option.textContent = setting.name;
+    difficultySelect.appendChild(option);
+});
 export {};
-/*
-Easy: 9×9 tiles, 10 mines
-Medium: 16×16 tiles, 40 mines
-Hard: 30×16 tiles, 99 mines
-*/ 
+// Initialization
 //# sourceMappingURL=minesweeper.js.map
--- minesweeper.ts
+++ minesweeper.ts
@@ -1,5 +1,28 @@
-/*
-Easy: 9×9 tiles, 10 mines
-Medium: 16×16 tiles, 40 mines
-Hard: 30×16 tiles, 99 mines
-*/
+// Interfaces
+interface DifficultySetting {
+    name: string;
+    size: {
+        width: number;
+        height: number;
+    };
+    numberOfMines: number;
+}
+
+// Variables & DOM selections
+const DIFFICULTIES: Record<string, DifficultySetting> = {
+    easy:   { name: 'Easy',   size: { width: 9,  height: 9  }, numberOfMines: 10 },
+    medium: { name: 'Medium', size: { width: 16, height: 16 }, numberOfMines: 40 },
+    hard:   { name: 'Hard',   size: { width: 30, height: 16 }, numberOfMines: 99 },
+};
+
+const difficultySelect = document.getElementById('difficulty') as HTMLSelectElement;
+
+// Game logic, functions, event listeners
+Object.entries(DIFFICULTIES).forEach(function ([key, setting]) {
+    const option = document.createElement('option');
+    option.value = key;
+    option.textContent = setting.name;
+    difficultySelect.appendChild(option);
+});
+
+// Initialization
